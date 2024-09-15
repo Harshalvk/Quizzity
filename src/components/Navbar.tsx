cd9f4d22d53a4086e77fa17bca184ede46b148ef
Harshalvk <harshalvkhobragade@gmail.com>
2024-09-16 00:59:16 +0530

    FIX: Remove empty props

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,7 @@ import React from "react";
 import SignInButton from "./SignInButton";
 import UserAccountNav from "./UserAccountNav";
 
-type Props = {};
-
-const Navbar = async (props: Props) => {
+const Navbar = async () => {
   const session = await getAuthSession();
   return (
     <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
