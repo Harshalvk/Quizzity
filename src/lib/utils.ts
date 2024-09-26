28fa50d82812bdfff054569c1d0e9e6dff6dc7c7
Harshalvk <harshalvkhobragade@gmail.com>
2024-09-26 17:57:19 +0530

    ADD: Utility function to calculate & format time

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const formateTimeDelta = (seconds: number) => {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds - hours * 3600) / 60);
+  const sec = Math.floor(seconds - hours * 3600 - minutes * 60);
+  const parts = [];
+
+  if (hours > 0) {
+    parts.push(`${hours}h`);
+  }
+  if (sec > 0) {
+    parts.push(`${sec}s`);
+  }
+  if (minutes > 0) {
+    parts.push(`${minutes}m`);
+  }
+
+  return parts.join(" ");
+};
